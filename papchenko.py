f64efad44839daf4c96756e2a1da52721dd8f8b7
DariaYurkova <146206485+DariaYurkova@users.noreply.github.com>
2024-01-27 19:15:54 +0300

    Add files via upload

---
+++ papchenko.py
@@ -0,0 +1,18 @@
+
+dog = { 'name': 'Хороший пёсик',
+        'height': 75,
+        'weight':40,
+        'food':'мясо' }
+def run(object):
+    print(f"{object['name']} бегает")
+    run(dog)
+
+    class Dog: def
+
+def __init__(self, name, height, weight, food='Нормальная'):
+    (self.name) = name
+    self.height = height
+    self.weight = weight
+    self.food = food
+    def run(self):
+    print(f"{self.name} бегает")tommy = Dog('Томми',75,40) emma = Dog('Емма',20,5)
